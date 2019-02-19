de30b793b4b460d7b0205ac7dd9aa20e120f9f36
leiliang <leiliang@youedata.com>
2019-02-19 14:11:33 +0800

    我的博客

---
+++ blog/src/components/AddBlog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="add-blog">
+      <h2>Add Blog</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Add-blog',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
